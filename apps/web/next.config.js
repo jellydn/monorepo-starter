9c6e4b5b5dbb81bb487162b456a1c446f45ff059
jellydn <870029+jellydn@users.noreply.github.com>
2025-03-14 16:02:46 +0000

    Apply automatic changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["@repo/ui"],
-  output: "standalone",
+	reactStrictMode: true,
+	transpilePackages: ["@repo/ui"],
+	output: "standalone",
 };
